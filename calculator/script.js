36702997f6d6c9bca30d2726b16f0a72cb3aeaaf
Satyamjha <167025454+Satyamjha2604@users.noreply.github.com>
2025-07-20 23:49:11 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,8 @@
+let str = "";
+let buttons = document.querySelectorAll(".button");
+Array.from(buttons).forEach((button) => {
+  button.addEventListener("click", (e) => {
+    console.log(e.target);
+    string = string + e.target.innerHTML;
+  });
+});
